23b4e89a1e9d515ada218b6f8885049251834695
Anneloes <anneloes9405@hotmail.com>
2020-05-09 17:30:12 -0400

    installed semantic-ui-react, set up menu bar
    Signed-off-by: Anneloes <anneloes9405@hotmail.com>

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React from 'react';
+import Menu from './components/Menu.js'
 import './App.scss';
 
 export default function App() {
   return (
-    <main className="layout">
-      <section className="sidebar">
-        <div>I AM THE SIDEBAR</div>
-        <nav className="sidebar__menu">
-          <span>hello I am a category</span>
-          <span>hello I am another category</span>
-        </nav>
-      </section>
-      <section className="food-content">
-        Hello, I am a the main section
-      </section>
-    </main>
+    <div>
+      <Menu />
+      <main>
+        <section className="food-content">
+          Hello, I am a the main section
+        </section>
+      </main>
+    </div>
   );
 }
